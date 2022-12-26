38aed96e0aacc6a1060509992d833031bc3ec581
Roman Okatev <43115066+Nebulawalker@users.noreply.github.com>
2022-12-26 23:58:58 +0300

    download books from tululu.org.

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import requests
+import os
 
+book_link = 'https://tululu.org/txt.php'
 
-book_link = 'https://tululu.org/txt.php?id=32168'
 
 def main():
-    response = requests.get(book_link)
-    response.raise_for_status()
-    filename = '1.txt'
-    with open(filename, 'wb') as file:
-        file.write(response.content)
+    os.makedirs('books', exist_ok=True)
+
+    for index in range(0, 10):
+        payload = {'id': index}
+        response = requests.get(book_link, params=payload)
+        response.raise_for_status()
+        path = os.path.join('books', f'id {index}.txt')
+        with open(path, 'wb') as file:
+            file.write(response.content)
 
 
 if __name__ == '__main__':
