4928e783d5fd7f3672741c8b543b231008c2d10b
Roman Okatev <43115066+Nebulawalker@users.noreply.github.com>
2022-12-29 00:37:15 +0300

    download comments. Refactoring

--- main.py
+++ main.py
@@ -24,13 +24,13 @@ def main():
         book_description = BookParser(TULULU_URL, index)
         download_txt(
             correct_url,
-            f'{index}. {book_description.book_title}'
+            f'{index}. {book_description.get_book_title()}'
         )
-        download_image(book_description.book_image_url)
+        download_image(book_description.get_book_cover_url())
 
-        print(f'Заголовок: {book_description.book_title}')
-        print(book_description.book_image_url)      
-        
+        print(f'Заголовок: {book_description.get_book_title()}')
+        print(book_description.get_book_cover_url())      
+        print(book_description.get_book_comments())
 
 if __name__ == '__main__':
     main()
--- parsers.py
+++ parsers.py
@@ -8,9 +8,24 @@ class BookParser:
         self.book_html_page = requests.get(f'{urljoin(url, f"b{book_id}")}')
         self.book_html_page.raise_for_status
         self.soup = BeautifulSoup(self.book_html_page.text, 'lxml')
-        self.book_title = self.soup.find('h1').text.split('::')[0].strip()
-        self.book_image_url = urljoin(
+
+    def get_book_title(self):
+        book_title = self.soup.find('h1').text.split('::')[0].strip()
+        return book_title
+
+    def get_book_cover_url(self):
+        book_cover_url = urljoin(
             self.book_html_page.url,
             self.soup.find('div', class_='bookimage').find('img')['src']
         )
-    
+        return book_cover_url
+
+    def get_book_comments(self):
+        raw_comments = self.soup.find_all('div', class_='texts')
+        book_comments = []
+        for raw_comment in raw_comments:
+            book_comments.append(raw_comment.find('span').text)
+        return '\n'.join(book_comments)
+
+
+
