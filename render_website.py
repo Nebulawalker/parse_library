2090a5ee69516054a223e023be6328a900d2a63a
Roman Okatev <43115066+Nebulawalker@users.noreply.github.com>
2023-04-16 15:52:14 +0300

    Integrate livereload.

--- render_website.py
+++ render_website.py
@@ -1,25 +1,31 @@
 import json
 
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+from livereload import Server
 
-with open('books.json', 'r', encoding='utf-8') as file:
-    books = json.load(file)
 
-template = env.get_template('template.html')
+def render_page():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-rendered_page = template.render(
-    books=books,
-)
+    with open('books.json', 'r', encoding='utf-8') as file:
+        books = json.load(file)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    template = env.get_template('template.html')
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    rendered_page = template.render(
+        books=books,
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+
+if __name__ == '__main__':
+    render_page()
+    server = Server()
+    server.watch('template.html', render_page)
+    server.serve(root='.')
